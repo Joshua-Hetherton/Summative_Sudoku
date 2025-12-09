797a2f59369c4716b68c61bd03b275a39ec50378
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2025-12-09 11:14:50 +0000

    Basic Outline of Forward Chaining Rules and Facts

--- Foward_Chaining_P2.py
+++ Foward_Chaining_P2.py
@@ -1,3 +1,46 @@
+"""
+Users can change the facts at any point, allowing the user to test diagnosing different problems
+Facts:
+    #Power
+    battery_voltage=0-50V
+    battery_charge=0-100%
+    solar_panel_voltage= 0-50V
+    solar_panel_current=0-10A
+    capacitor_voltage=0-50V
+
+    #Temperature/ Thermal
+    outside_temperature = -150C - 120C
+    internal_temperature= 30-70C
+
+    #Communication
+    antenna_signal= strong, weak, none
+    antenna_orientation= earth_facing, space_facing, unknown
+
+    #Attitude Control
+    attitude_stable= True/False
+    reaction_wheel= 0-6000RPM
+
+    #Onboard Computer
+    CPU_temp= 0-100C
+    CPU_usage= 0-100%
+    memory_usage= 0-100%
+    storage_available=0-100%
+
+
+Rules:
+    #== Power System Rules ==#
+
+    #== Thermal Rules ==#
+
+    #== Communication System Rules ==#
+
+    #== Attitude Control Rules ==#
+
+    #== Onboard Computer Rules ==#
+"""
+
+
+
 def forward_chaining():
     pass
 
